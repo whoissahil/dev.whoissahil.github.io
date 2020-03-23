da784307475972af8b3de01d1eaac0cb2897f4ad
Sahil Sharma <me@iamsahilsharma.com>
2020-03-23 17:49:54 +0400

    Updates

---
+++ precache-manifest.6be90fd4d3fc2dcce5745bf036559f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2fde180ab7fa652fc14a79e9210f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef39df117fcc313ed2c9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bde5aaa583bcfccbff9",
+    "url": "/static/js/2.45750398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45750398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef39df117fcc313ed2c9",
+    "url": "/static/js/main.798d622f.chunk.js"
+  },
+  {
+    "revision": "6ba47183e774a322e5b8",
+    "url": "/static/js/runtime-main.9cd8d1e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
